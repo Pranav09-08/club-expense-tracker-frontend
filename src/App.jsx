94117aab7d8dc03a588bef075c637d2f215d536b
pranav <pranavmahale08@gmail.com>
2026-02-24 10:18:27 +0530

    Shadcn Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
-  
   return (
-     <h1 className="text-9xl font-bold text-center">Hello</h1>
+    <div className="min-h-screen flex items-center justify-center">
+      <Button>shadcn Ready 🚀</Button>
+    </div>
   )
 }
 
